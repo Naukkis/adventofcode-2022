f41278f95e4763c0664086560bc1c8e68627b40f
Ville Naukkarinen <naukkis@gmail.com>
2022-12-06 12:40:42 +0200

    day 4 part 2

--- day4.py
+++ day4.py
@@ -1,8 +1,7 @@
 from input_reader import read_input
 
-if __name__ == '__main__':
-    input_data = read_input(4, str, False)
 
+def part1(input_data):
     fully_contained_count = 0
     for pairs in input_data:
         pair1, pair2 = pairs.split(',')
@@ -30,5 +29,42 @@ if __name__ == '__main__':
 
     print(fully_contained_count)
 
-    # part1(input_data)
-    # part2(input_data)
+
+def part2(input_data):
+    overlapped_count = 0
+    for pairs in input_data:
+        pair1, pair2 = pairs.split(',')
+        pair1_start_end = pair1.split('-')
+        pair2_start_end = pair2.split('-')
+        first_pair_sections = []
+        second_pair_sections = []
+        if pair1_start_end[0] == pair1_start_end[1]:
+            first_pair_sections.append(int(pair1_start_end[0]))
+        else:
+            for section in range(int(pair1_start_end[0]), int(pair1_start_end[1]) + 1):
+                first_pair_sections.append(section)
+        if pair2_start_end[0] == pair2_start_end[1]:
+            second_pair_sections.append(int(pair2_start_end[0]))
+        else:
+            for section in range(int(pair2_start_end[0]), int(pair2_start_end[1]) + 1):
+                second_pair_sections.append(section)
+
+        overlapFound = False
+        for section in first_pair_sections:
+            if section in second_pair_sections:
+                overlapped_count = overlapped_count + 1
+                overlapFound = True
+                break
+
+        if not overlapFound:
+            for section in second_pair_sections:
+                if section in first_pair_sections:
+                    overlapped_count = overlapped_count + 1
+                    break
+    print(overlapped_count)
+
+
+if __name__ == '__main__':
+    input_data = read_input(4, str, False)
+    part1(input_data)
+    part2(input_data)

763d0d3abf39c8bcdc26377a00db4936ff9734ab
Ville Naukkarinen <naukkis@gmail.com>
2022-12-06 12:12:53 +0200

    day 3 part 2

--- day3.py
+++ day3.py
@@ -2,7 +2,7 @@ from input_reader import read_input
 
 
 def part1(rucksacks):
-    prioritiesLower = {chr(ord('a') + i): i + 1 for i in range(26)}
+    priorities_lower = {chr(ord('a') + i): i + 1 for i in range(26)}
 
     duplicates = 0
     for rucksack in rucksacks:
@@ -13,12 +13,33 @@ def part1(rucksacks):
             if char in comp2:
                 if char.isupper():
                     lower_case = char.lower()
-                    duplicates = duplicates + prioritiesLower[lower_case] + 26
+                    duplicates = duplicates + priorities_lower[lower_case] + 26
                 else:
-                    duplicates = duplicates + prioritiesLower[char]
+                    duplicates = duplicates + priorities_lower[char]
+    print(duplicates)
+
+
+def part2(rucksacks):
+    priorities_lower = {chr(ord('a') + i): i + 1 for i in range(26)}
+
+    duplicates = 0
+    for index in range(0, len(rucksacks), 3):
+        first_set = set(rucksacks[index])
+        second_set = set(rucksacks[index + 1])
+        third_set = set(rucksacks[index + 2])
+
+        for char in first_set:
+            if char in second_set:
+                if char in third_set:
+                    if char.isupper():
+                        lower_case = char.lower()
+                        duplicates = duplicates + priorities_lower[lower_case] + 26
+                    else:
+                        duplicates = duplicates + priorities_lower[char]
     print(duplicates)
 
 
 if __name__ == '__main__':
     input_data = read_input(3, str, False)
     part1(input_data)
+    part2(input_data)
